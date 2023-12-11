3af64fb2125fc744e1b69f3a09011c7dcefefe3f
Ramji <100006991+javarangers@users.noreply.github.com>
2023-12-11 09:51:06 -0500

    adding another println in java code

--- PracticeGitJava2/src/com/second/git/SecondGitClassPractice.java
+++ PracticeGitJava2/src/com/second/git/SecondGitClassPractice.java
@@ -3,6 +3,7 @@ package com.second.git;
 public class SecondGitClassPractice {
 	public static void main(String[] args) {
 		System.out.println("Hello my friend how are you doing?");
+		System.out.println("Hello friend it is snowing outside today");
 	
 	}
 }
